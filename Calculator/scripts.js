ed5b4213a3bc3ae5a308d1569bce8df814d515f5
Fabian Portilla <flportilla@gmail.com>
2022-05-25 15:47:57 -0500

    Change the selector for JS and CSS

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+//Selectors
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+
+
+
+
+//Event Listeners
+
+
