3530b618b2630097017a8b7d422bb22c5ff31dd8
Fabian Portilla <flportilla@gmail.com>
2022-05-25 16:26:33 -0500

    Add basic functionality to the operators and numbers

--- Calculator/scripts.js
+++ Calculator/scripts.js
@@ -1,13 +1,64 @@
 //Selectors
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation]');
+const changeSign = document.querySelector('[data-changeSign]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
-const allClearButton = document.querySelector('[data-all-clear]');
+const allClearButton = document.querySelector('[data-clearAll]');
+const display = document.querySelector('[data-display]');
 
 
+//Functions
 
+function showNumber(){
+    const number = this.id
+    const maxLength = display.value.length;
+    if(maxLength < 14){
+    display.value += number;
+    }
+}
+
+function clearAll(){
+    display.value = '';
+}
+
+function deleteLast(){
+    display.value = display.value.slice(0, -1);
+}
+
+function negative(){
+    display.value = display.value * -1;
+}
+
+function showOperator(){
+
+    if(display.value === '') return;
+
+    if(display.value.slice(-1) === '+' || display.value.slice(-1) === '-' || display.value.slice(-1) === '*' || display.value.slice(-1) === '/') return;
+
+    if(this.id === '+'){
+        display.value += '+';
+    }
+    else if(this.id === '-'){
+        display.value += '-';
+    }
+    else if(this.id === '*'){
+        display.value += '*';
+    }
+    else if(this.id === '/'){
+        display.value += '/';
+    }
+
+}
 
 //Event Listeners
 
-
+numberButtons.forEach(button => {
+    button.addEventListener('click', showNumber);
+})
+allClearButton.addEventListener('click', clearAll);
+deleteButton.addEventListener('click', deleteLast);
+changeSign.addEventListener('click', negative);
+operationButtons.forEach(button => {
+    button.addEventListener('click', showOperator);
+})
