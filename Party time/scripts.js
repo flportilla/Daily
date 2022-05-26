68f6db1d05d9b09e018a388818b322c700172228
Fabian Portilla <flportilla@gmail.com>
2022-05-26 18:14:58 -0500

    Add the results of the logic to the HTML document

--- Party time/scripts.js
+++ Party time/scripts.js
@@ -4,6 +4,8 @@ const birthDay = document.querySelector('[data-bDay]');
 const birthTime = document.querySelector('[data-bTime]');
 const submitButton = document.querySelector('[data-bButton]');
 const form = document.querySelector('[data-form]');
+const birthDayList = document.querySelector(".birtday_info");
+const timeList = document.querySelector(".time_info");
 let birthDayInfo;
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
@@ -17,6 +19,7 @@ function getBdayInfo(submitAction) {
     const milisecsToDays = (1000 * 60 * 60 * 24);//Convert miliseconds to days = 1000 milliseconds * 60 seconds in a minute * 60 minutes in an hour * 24 hours in a day
     const todayYear = new Date().getFullYear(); //Current year
     const todayDays = Math.floor((new Date().getTime()) / milisecsToDays); //get the number of days since 1/1/1970 
+    const todayMinutes = (new Date().getTime()) / (1000 * 60) //get the number of minutes since 1/1/1970
     const tenKDays = (10000 * 24 * 60 * 60 * 1000) //10,000 days in miliseconds
     const twentyKDays = (20000 * 24 * 60 * 60 * 1000) //20,000 days in miliseconds
     const fiveHundredThousandHours = (500000 * 60 * 60 * 1000) //500,000 hours in miliseconds
@@ -32,7 +35,7 @@ function getBdayInfo(submitAction) {
     const bDay = birthDay.value.slice(8, 10); //get the day from the input
 
     //calculate the age in years
-    const age = todayYear - bYear;
+    const yearsOld = todayYear - bYear;
 
     //calculate the age in days
     const month = months[bMonth - 1];
@@ -41,7 +44,7 @@ function getBdayInfo(submitAction) {
     const daysOld = Math.floor(todayDays - (dayOfBirth.getTime()) / milisecsToDays);
 
     //calculate the age in minutes
-    const minutesOld = Math.floor((todayDays - (dayOfBirth.getTime()) / milisecsToDays) * 1440);
+    const minutesOld = (Math.floor((todayMinutes - (dayOfBirth.getTime()) / (1000 * 60))))-360;
 
     //calculate the day #10000
     const day10000 = new Date(dayOfBirth.getTime() + tenKDays);
@@ -67,20 +70,49 @@ function getBdayInfo(submitAction) {
     return birthDayInfo = {
         month,
         day: bDay,
-        age,
+        yearsOld,
         daysOld,
-        dayTenThousand: `${days[day10000.getDay()]} ${months[day10000.getMonth()]} ${day10000.getDate()} ${day10000.getFullYear()}`,
-        dayTwentyThousand: `${days[day20000.getDay()]} ${months[day20000.getMonth()]} ${day20000.getDate()} ${day20000.getFullYear()}`,
-        hour500000: `${days[hour500000.getDay()]} ${months[hour500000.getMonth()]} ${hour500000.getDate()} ${hour500000.getFullYear()}`,
-        minute1000000: `${days[minute1000000.getDay()]} ${months[minute1000000.getMonth()]} ${minute1000000.getDate()} ${minute1000000.getFullYear()}`,
-        minute10000000: `${days[minute10000000.getDay()]} ${months[minute10000000.getMonth()]} ${minute10000000.getDate()} ${minute10000000.getFullYear()}`,
-        minute50000000: `${days[minute50000000.getDay()]} ${months[minute50000000.getMonth()]} ${minute50000000.getDate()} ${minute50000000.getFullYear()}`,
-        second1000000000: `${days[second1000000000.getDay()]} ${months[second1000000000.getMonth()]} ${second1000000000.getDate()} ${second1000000000.getFullYear()}`
+        minutesOld,
+        day10000,
+        day20000,
+        hour500000,
+        minute1000000,
+        minute10000000,
+        minute50000000,
+        second1000000000,
 
     }
 
     //is or was
 }
 
+function showInfo(submitEvent){
+    getBdayInfo(submitEvent);
+    
+    //create the list with the info of the birthday
+    birthDayList.innerHTML = `
+            <li>Your birthday is ${birthDayInfo.month} ${birthDayInfo.day}</li>
+            <li>You're ${birthDayInfo.yearsOld} years old</li>
+            <li>You're ${birthDayInfo.daysOld} days old</li>
+            <li>You're ${birthDayInfo.minutesOld} minutes old</li>
+    `;
+
+    //create the list with the info of the time, basically play around with seconds, hours, days and months
+    timeList.innerHTML = `
+            <li>Your 10,000th day ${(new Date().getTime() - birthDayInfo.day10000.getTime()) < 0 ? "is" : "was" } ${days[birthDayInfo.day10000.getDay()]} ${months[birthDayInfo.day10000.getMonth()]} ${birthDayInfo.day10000.getDate()} ${birthDayInfo.day10000.getFullYear()}<li>
+
+            <li>Your 20,000th day ${(new Date().getTime() - birthDayInfo.day20000.getTime()) < 0 ? "is" : "was" } ${days[birthDayInfo.day20000.getDay()]} ${months[birthDayInfo.day20000.getMonth()]} ${birthDayInfo.day20000.getDate()} ${birthDayInfo.day20000.getFullYear()}</li>
+
+            <li>Your 500,000th day ${(new Date().getTime() - birthDayInfo.hour500000.getTime()) < 0 ? "is" : "was" } ${days[birthDayInfo.hour500000.getDay()]} ${months[birthDayInfo.hour500000.getMonth()]} ${birthDayInfo.hour500000.getDate()} ${birthDayInfo.hour500000.getFullYear()}</li>
+
+            <li>Your one millionth minute ${(new Date().getTime() - birthDayInfo.minute1000000.getTime()) < 0 ? "is" : "was" } ${days[birthDayInfo.minute1000000.getDay()]} ${months[birthDayInfo.minute1000000.getMonth()]} ${birthDayInfo.minute1000000.getDate()} ${birthDayInfo.minute1000000.getFullYear()}</li>
+
+            <li>Your ten millionth minute ${(new Date().getTime() - birthDayInfo.minute10000000.getTime()) < 0 ? "is" : "was" } ${days[birthDayInfo.minute10000000.getDay()]} ${months[birthDayInfo.minute10000000.getMonth()]} ${birthDayInfo.minute10000000.getDate()} ${birthDayInfo.minute10000000.getFullYear()}</li>
+
+            <li>Your fifty millionth minute ${(new Date().getTime() - birthDayInfo.minute50000000.getTime()) < 0 ? "is" : "was" } ${days[birthDayInfo.minute50000000.getDay()]} ${months[birthDayInfo.minute50000000.getMonth()]} ${birthDayInfo.minute50000000.getDate()} ${birthDayInfo.minute50000000.getFullYear()}</li>
+                `;
+    
+}
+
 //add event listener
-form.addEventListener('submit', getBdayInfo);
+form.addEventListener('submit', showInfo);
