b39f2c7c26c1667f5ccc51c943701dbcafec1e43
Fabian Portilla <flportilla@gmail.com>
2022-05-27 11:19:25 -0500

    Add the function to get the words list into an array

--- Scrambled!/scripts.js
+++ Scrambled!/scripts.js
@@ -3,10 +3,21 @@ const instructionsContainer = document.querySelector('[data-instructions_contain
 const gameContainer = document.querySelector('[data-game_container]');
 const playButton = document.querySelector('[data-play_button]');
 
+//Get the words list
+let wordsList = []
+window.addEventListener('load', async () => {
+
+    const endpoint = "https://gist.githubusercontent.com/flportilla/15434508547bdcdfa58f4314a8704ef0/raw/24510856f6c566d99fcb6e8e740569c4063f2f3f/Words%2520List"
+
+    const wordList =
+        await fetch(endpoint)
+            .then(blob => blob.json());
+
+    return wordsList = wordList.filter(word => word.length >= 4);
+    })
 
 
-
-// List of random words .JSON https://raw.githubusercontent.com/bevacqua/correcthorse/master/wordlist.json
+// List of random words .JSON 
 
 
 function startGame() {
@@ -18,3 +29,6 @@ function startGame() {
 
 //Add event listeners
 playButton.addEventListener('click', startGame);
+
+
+
