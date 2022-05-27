4980bbb376a41ccee7d19c6a29f8c60ab2c454c3
Fabian Portilla <flportilla@gmail.com>
2022-05-27 11:09:02 -0500

    Add the show/hide function for the game/instructions container

--- Scrambled!/scripts.js
+++ Scrambled!/scripts.js
@@ -1,3 +1,20 @@
-console.log("Hello World");
+//Selectors
+const instructionsContainer = document.querySelector('[data-instructions_container]');
+const gameContainer = document.querySelector('[data-game_container]');
+const playButton = document.querySelector('[data-play_button]');
+
+
+
 
 // List of random words .JSON https://raw.githubusercontent.com/bevacqua/correcthorse/master/wordlist.json
+
+
+function startGame() {
+    // Hide the instructions container
+    instructionsContainer.style.display = 'none';
+    // Show the game container
+    gameContainer.style.display = 'flex';
+}
+
+//Add event listeners
+playButton.addEventListener('click', startGame);
