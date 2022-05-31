08c7f4d957656ddfd83f2dd30856928a848b14cd
Fabian Portilla <flportilla@gmail.com>
2022-05-31 17:07:27 -0500

    First attempt, failed

--- BuildingBlocks/scripts.js
+++ BuildingBlocks/scripts.js
@@ -1,32 +1,55 @@
 //Selectors
 
 const shapes = document.querySelectorAll('.shape');
+const toyBox = document.querySelector('#toy-box');
 const canvas = document.querySelector('#canvas');
 const infoContainer = document.querySelector('#info-container');
 let currentShape;
 let interval1;
+let isDragging = false;
 
 //Variables
+
+const mouseCoords = {
+    initialX: 0,
+    initialY: 0,
+
+    newX: 0,
+    newY: 0
+} 
+
 
 
 //Functions
 
-//Get the shape that is clicked, clone it, and add it to the page
-function handleInitialValues(e) {
-    currentShape = e.target;
+//Get the shape that is clicked, clone it, and add it to the #canvas div
+function handleInitialValues(mouseDownAction) {
+
+    //Get the shape that is clicked
+    currentShape = mouseDownAction.target;
+
+    //Clone the shape
     const shapeSelected = currentShape.cloneNode(true);
+
+    //Add the cloned shape to the #canvas div
     canvas.appendChild(shapeSelected);
+
+    isDragging = true;
 
 }
 
+//Hide info container and show the canvas
 function showCanvas() {
 
     infoContainer.classList.add('hidden');
+
     canvas.classList.remove('hidden');
     canvas.classList.add('canvas');
+
     interval1 = setInterval(() => {
         infoContainer.classList.add('hide');
     }, 1000);
+
 }
 
 //Event Listeners
@@ -34,12 +57,6 @@ shapes.forEach(shape => {
     shape.addEventListener('click', handleInitialValues)
 });
 shapes.forEach(shape => {
-    shape.addEventListener('click', showCanvas)
-});
-
-//stop the interval on animation end
-canvas.addEventListener('animationend', () => {
-    clearInterval(interval1);
-
+    shape.addEventListener('mousedown', showCanvas)
 });
 
