84fbd17c054a440f43615309f51c6980947b1976
Fabian Portilla <flportilla@gmail.com>
2022-06-03 15:59:19 -0500

    Add counter functionality

--- click-challenge/scripts.js
+++ click-challenge/scripts.js
@@ -1,11 +1,23 @@
 //Selectors
-const startGameButton = document.querySelector('#start_game_button');
-const instructionsContainer = document.querySelector('#instructions_container');
+const startGameButton = document.getElementById('start_game_button');
+const instructionsContainer = document.getElementById('instructions_container');
+const clicks_counter = document.getElementById('clicks_counter')
+const gameContainer = document.getElementById('game_container');
+
+
 //Variables
+let score = 0;
 
 //Functions
 
 //Events
-startGameButton.addEventListener('click', function(){
+startGameButton.addEventListener('click', function () {
     instructionsContainer.style.display = 'none';
+    gameContainer.style.display = 'block';
+})
+document.addEventListener('click', function () {
+    if (gameContainer.style.display === 'block') {
+        score++;
+        clicks_counter.innerHTML = `${score}`;
+    }
 })
