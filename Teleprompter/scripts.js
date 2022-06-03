1db571ef58b8271a0ef395c12618478c14211a6e
Fabian Portilla <flportilla@gmail.com>
2022-06-02 20:04:39 -0500

    Add styles when click the "teleprompt me" button

--- Teleprompter/scripts.js
+++ Teleprompter/scripts.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+//Selectors
+
+const submitButton = document.getElementById('button');
+const mainContainer = document.getElementById('main_container');
+const header = document.getElementById('header');
+const body = document.getElementById('body');
+
+//Functions 
+
+
+
+/**
+ * @param {string} text
+ */
+function showTeleprompter(text) {
+    header.classList.add('dark')
+    body.classList.add('dark_background')
+    mainContainer.classList.add('dark_background')
+    mainContainer.innerHTML = `
+    <div class="teleprompt_screen">${text}</div>
+    `;
+}
+
+//Event Listeners
+
+submitButton.addEventListener('click', (clickEvent) => {
+    clickEvent.preventDefault();
+    const textAreaValue = document.getElementById('textarea').value;
+    showTeleprompter(textAreaValue);
+})
