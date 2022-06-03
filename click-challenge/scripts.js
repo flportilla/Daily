19c3655967cb44a15100efe4786eedec53b79e90
Fabian Portilla <flportilla@gmail.com>
2022-06-03 16:28:26 -0500

    Add score results after 30 seconds

--- click-challenge/scripts.js
+++ click-challenge/scripts.js
@@ -3,21 +3,41 @@ const startGameButton = document.getElementById('start_game_button');
 const instructionsContainer = document.getElementById('instructions_container');
 const clicks_counter = document.getElementById('clicks_counter')
 const gameContainer = document.getElementById('game_container');
-
+const tryAgain = document.getElementById('start_again_container');
+const totalClicks = document.getElementById('total_clicks');
+const clicksPerSecond = document.getElementById('clicks_per_second');
 
 //Variables
 let score = 0;
+let gameStart = false;
 
 //Functions
-
-//Events
-startGameButton.addEventListener('click', function () {
+function startGame(){
+    gameStart = true;
     instructionsContainer.style.display = 'none';
     gameContainer.style.display = 'block';
-})
-document.addEventListener('click', function () {
-    if (gameContainer.style.display === 'block') {
+    handleGameDuration();
+}
+
+function clickCounter(){
+    if (gameStart) {
         score++;
         clicks_counter.innerHTML = `${score}`;
     }
-})
+}
+
+function handleGameDuration(){
+    setTimeout(() => {
+        gameStart = false;
+        tryAgain.style.display = 'block';
+        gameContainer.style.display = 'none';
+        totalClicks.innerHTML = `${score}`;
+        const clickPerSecond = (score / 30).toFixed(2);
+        clicksPerSecond.innerHTML = `${clickPerSecond}`;
+        score = 0;
+    } , 30000);
+}
+
+//Events
+startGameButton.addEventListener('click', startGame);
+document.addEventListener('click', clickCounter);
