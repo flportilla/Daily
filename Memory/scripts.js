0dd2daf7d1395bb35183668c2da6ea4b700afbec
Fabian Portilla <flportilla@gmail.com>
2022-06-08 12:55:28 -0500

    Add checkMatch functionality

--- Memory/scripts.js
+++ Memory/scripts.js
@@ -3,6 +3,7 @@ const attempts = document.getElementById('attempts');
 const matches = document.getElementById('matches');
 const cards = document.querySelectorAll(".card")
 const cardsBack = document.querySelectorAll(".card_back")
+const cardsContainer = document.querySelector(".cards_container")
 
 //Variables
 const scores = {
@@ -12,25 +13,77 @@ const scores = {
 const imgSources = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
 ]
-const cardsSelected = {
-    first: null,
-    second: null
-}
+
+const flippedCards = []
 
 //Functions
 
-function firstCardHandler(clickEvent) {
-    const cardSelected = clickEvent.target
-    const cardSelectedBack = cardSelected.nextElementSibling
+function cardFlipHandler(clickEvent) {
 
-    cardSelected.style.display = "none"
-    cardSelectedBack.style.display = "block"
+    //Save the clicked card
+    const cardBack = clickEvent.target
+    const cardSelected = cardBack.nextElementSibling
 
-    cardsSelected.first = cardSelected
+    //Add the clicked card to the flipped cards array
+    flippedCards.push(cardSelected)
+
+    //Flip the card
+    cardBack.style.display = "none"
+    cardSelected.style.display = "block"
+
+    //keep the cards array in a length of 2
+    flippedCards.splice(0, flippedCards.length - 2);
+
+    //Check if the cards match
+    handleMatch(flippedCards)
 }
 
-function generateRandomGrid(imagesList) {
+function handleMatch(flippedCards) {
 
+    //if the array is full
+    if (flippedCards.length === 2) {
+
+        //Disable clicks
+        cardsContainer.style.pointerEvents = "none"
+
+        //Add attempts and update the attempts counter
+        scores.attempts++
+        attempts.innerText = `Attempts: ${scores.attempts}`
+
+        //if the cards match
+        if (flippedCards[0].src === flippedCards[1].src) {
+
+            //Add matches and update the matches counter
+            scores.matches++
+            matches.innerText = `Matches: ${scores.matches}`
+
+            //remove the cards from the array
+            flippedCards.length = 0
+
+            //Enable clicks again
+            cardsContainer.style.pointerEvents = "auto"
+
+        }
+
+        //if the cards don't match
+        else {
+
+            //Wait 400ms before flipping the cards back
+            setTimeout(() => {
+                flippedCards.forEach(card => {
+                    card.style.display = "none"
+                    card.previousElementSibling.style.display = "block"
+                })
+                flippedCards.length = 0
+                cardsContainer.style.pointerEvents = "auto"
+            }, 400)
+        }
+    }
+
+}
+
+//generate the cards randomly 
+function generateRandomGrid(imagesList) {
     const copyOfImagesList = imagesList.slice()
     cards.forEach(card => {
         const randomIndex = Math.floor(Math.random() * copyOfImagesList.length)
@@ -41,11 +94,9 @@ function generateRandomGrid(imagesList) {
 
 //Event listeners
 cardsBack.forEach(cardBack => {
-    cardBack.addEventListener('click', firstCardHandler)
+    cardBack.addEventListener('click', cardFlipHandler)
 
 })
-
-
 
 window.addEventListener("load", () => {
     generateRandomGrid(imgSources)
