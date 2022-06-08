6c3c6727f3ffb6cdfc8da1ccfd1be9f65ede8f17
Fabian Portilla <flportilla@gmail.com>
2022-06-08 17:45:06 -0500

    Add pop and superButton class change functionalities

--- Bubble Wrap/scripts.js
+++ Bubble Wrap/scripts.js
@@ -1,1 +1,52 @@
-console.log("Hello World");
+//Selectors
+const resetButton = document.getElementById("reset_button");
+const superButton = document.getElementById("super_button");
+const bubbleContainer = document.getElementById("bubble_container");
+const popSound = document.getElementById("pop_sound");
+const bubbles = document.querySelectorAll(".bubble");
+const poppedBubbles = document.querySelectorAll(".popped");
+
+
+//Variables
+let superFlag = false;
+//Functions
+function popBubble(clickEvent) {
+    clickEvent.target.style.display = "none";
+    clickEvent.target.parentNode.nextElementSibling.style.display = "block"
+    popSound.innerHTML = `
+        <audio autoplay = "">
+            <source src="./audio/pop.wav" type="audio/wav">
+        </audio>`;
+}
+
+function handleSuperButton(clickEvent) {
+
+    const superB = superButton.classList;
+
+    if (superB.value.includes("off")) {
+        superB.remove("off");
+        superB.add("on");
+        superFlag = true;
+    }
+    else {
+        superB.remove("on");
+        superB.add("off");
+        superFlag = false;
+    }
+}
+
+//Event Listeners
+
+bubbles.forEach(bubble => {
+    bubbles.forEach(bubble => {
+        bubble.addEventListener("click", popBubble);
+    })
+});
+
+resetButton.addEventListener("click", () => {
+    location.reload();
+});
+
+superButton.addEventListener("click", () => {
+    handleSuperButton();
+})
