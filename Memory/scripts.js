696b48dd38e0e2c6164be3e1199e8bc790fd697f
Fabian Portilla <flportilla@gmail.com>
2022-06-08 06:36:21 -0500

    Add the flipCard functionality on click

--- Memory/scripts.js
+++ Memory/scripts.js
@@ -1,40 +1,51 @@
 //Selectors
 const attempts = document.getElementById('attempts');
 const matches = document.getElementById('matches');
-const cards = document.querySelectorAll("[data-cards]")
+const cards = document.querySelectorAll(".card")
+const cardsBack = document.querySelectorAll(".card_back")
 
 //Variables
 const scores = {
     attempts: 0,
     matches: 0
 }
-const imgSources = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o']
-
+const imgSources = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o'
+]
+const cardsSelected = {
+    first: null,
+    second: null
+}
 
 //Functions
 
-function flipCard(clickEvent) {
-    const cardSelected = clickEvent.target.src
+function firstCardHandler(clickEvent) {
+    const cardSelected = clickEvent.target
+    const cardSelectedBack = cardSelected.nextElementSibling
+
+    cardSelected.style.display = "none"
+    cardSelectedBack.style.display = "block"
+
+    cardsSelected.first = cardSelected
 }
 
 function generateRandomGrid(imagesList) {
+
     const copyOfImagesList = imagesList.slice()
     cards.forEach(card => {
-
         const randomIndex = Math.floor(Math.random() * copyOfImagesList.length)
-
-        //check if the card is already used
-
-        card.src = `./img/${imgSources[randomIndex]}.jpg`
+        card.src = `./img/${copyOfImagesList[randomIndex]}.jpg`
         copyOfImagesList.splice(randomIndex, 1)
-        console.log(copyOfImagesList)
     })
 }
 
 //Event listeners
-cards.forEach(card => {
-    card.addEventListener('click', flipCard)
+cardsBack.forEach(cardBack => {
+    cardBack.addEventListener('click', firstCardHandler)
+
 })
+
+
 
 window.addEventListener("load", () => {
     generateRandomGrid(imgSources)
