f36f218690824d7a23f8e0cdbc6b1cf234b84277
Fabian Portilla <flportilla@gmail.com>
2022-06-10 14:44:32 -0500

    Add logic to find the winner

--- reactrps/src/App.js
+++ reactrps/src/App.js
@@ -1,10 +1,20 @@
 
+// @ts-ignore
 import left_fist from './left_fist.png';
+// @ts-ignore
 import right_fist from './right_fist.png';
+// @ts-ignore
 import rock from './rock.png';
+// @ts-ignore
 import paper from './paper.png';
+// @ts-ignore
 import scissors from './scissors.png';
 import './App.css';
+
+let setCountdown;
+const selections = {}
+
+
 
 function GameContainers() {
 
@@ -35,9 +45,9 @@ function ButtonsContainer() {
   }
   return (
     <>
-      <button className={buttonsName.rock} id={buttonsName.rock}>{buttonsName.rock}</button>
-      <button className={buttonsName.paper} id={buttonsName.paper}>{buttonsName.paper}</button>
-      <button className={buttonsName.scissors} id={buttonsName.scissors}>{buttonsName.scissors}</button>
+      <button type="button" onClick={handleShake} className={buttonsName.rock} id={buttonsName.rock}>{buttonsName.rock}</button>
+      <button type="button" onClick={handleShake} className={buttonsName.paper} id={buttonsName.paper}>{buttonsName.paper}</button>
+      <button type="button" onClick={handleShake} className={buttonsName.scissors} id={buttonsName.scissors}>{buttonsName.scissors}</button>
     </>
   )
 }
@@ -47,7 +57,7 @@ function Computer() {
       <div className="computer_name" id="computer_name">Computer</div>
       <div style={{ height: 100 }}></div>
       <div id='computer_hand'>
-        <img src={left_fist} id="computer_hand_img" alt="left_fist" />
+        <img src={left_fist} id="computer_hand_img" />
       </div>
     </>
   )
@@ -65,7 +75,7 @@ function Player() {
       <div className="player_name" id="player_name">You</div>
       <div style={{ height: 100 }}></div>
       <div id='player_hand'>
-        <img src={right_fist} id="player_hand_img" alt="right_fist" />
+        <img src={right_fist} onAnimationEnd={() => { handleChoices(); handleResults() }} id="player_hand_img" />
       </div>
     </>
   )
@@ -82,6 +92,125 @@ function App() {
       </div>
     </div>
   )
+}
+
+function handleChoices() {
+
+  const playerHand = document.getElementById('player_hand_img');
+  const computerSelection = Math.floor(Math.random() * 3) + 1;
+
+  switch (computerSelection) {
+    case 1:
+      selections.computer = 'rock';
+      break;
+    case 2:
+      selections.computer = 'paper';
+      break;
+    case 3:
+      selections.computer = 'scissors';
+      break;
+  }
+
+  switch (selections.player) {
+    case 'rock':
+      selections.player = 'rock';
+      playerHand.src = `${rock}`;
+      break;
+    case 'paper':
+      selections.player = 'paper';
+      playerHand.src = `${paper}`;
+      break;
+    case 'scissors':
+      selections.player = 'scissors';
+      playerHand.src = `${scissors}`;
+      break;
+  }
+}
+
+function setTimer() {
+  const results = document.getElementById('timer');
+
+  let contador = 2
+  results.innerText = '3';
+  const timer = setInterval(function () {
+    results.innerText = `${contador}`;
+    contador--;
+  }, 500);
+  setCountdown = timer;
+}
+
+function handleShake(clickEvent) {
+
+  const computerHand = document.getElementById('computer_hand_img');
+  const playerHand = document.getElementById('player_hand_img');
+
+  computerHand.src = `${left_fist}`
+  playerHand.src = `${right_fist}`
+
+  computerHand.classList.toggle('shake');
+  playerHand.classList.toggle('shake');
+
+  selections.player = clickEvent.target.id;
+
+  setTimer();
+};
+
+function handleResults() {
+  clearInterval(setCountdown)
+  const results = document.getElementById('timer');
+  const computerHand = document.getElementById('computer_hand_img');
+  const playerHand = document.getElementById('player_hand_img');
+
+  computerHand.classList.remove('shake');
+  playerHand.classList.remove('shake');
+
+  console.log(selections);
+
+  switch (selections.computer) {
+
+    case 'scissors':
+      computerHand.src = `${scissors} `
+
+      if (selections.computer === selections.player) {
+        results.innerText = `It's a draw!`;
+      }
+      else if (selections.player === 'rock' && selections.computer === 'scissors') {
+        results.innerText = `You win!`;
+      }
+      else if (selections.player === 'paper' && selections.computer === 'scissors') {
+        results.innerText = `You lose!`;
+      }
+      break;
+
+    case 'paper':
+      computerHand.src = `${paper}`
+
+      if (selections.computer === selections.player) {
+        results.innerText = `It's a draw!`;
+      }
+      else if (selections.player === 'rock' && selections.computer === 'paper') {
+        results.innerText = `You lose!`;
+      }
+      else if (selections.player === 'scissors' && selections.computer === 'paper') {
+        results.innerText = `You win!`;
+      }
+      break;
+
+    case 'rock':
+      computerHand.src = `${rock}`
+
+      if (selections.computer === selections.player) {
+        results.innerText = `It's a draw!`;
+      }
+      else if (selections.player === 'paper' && selections.computer === 'rock') {
+        results.innerText = `You win!`;
+      }
+      else if (selections.player === 'scissors' && selections.computer === 'rock') {
+        results.innerText = `You lose!`;
+      }
+      break;
+  }
+
 }
 
 export default App;
