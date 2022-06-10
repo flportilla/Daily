d05809ea062dcea7867efb0cec6b01db1b82006a
Fabian Portilla <flportilla@gmail.com>
2022-06-10 07:58:23 -0500

    rps React

--- Pong/scripts.js
+++ Pong/scripts.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+function FirstReverse(str) {
+    const reversedWord = str.split('').reverse().join('');
+    return reversedWord;
+
+}
+
+console.log(FirstReverse("hello"));
